a9a9777deb3811597ff8ec36171091eff264f3ec
Gharbi01 <114801428+Gharbi01@users.noreply.github.com>
2024-01-30 18:48:52 +0000

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { CarProps,FilterProps } from "@/Types";
 export async function fetchCars(filters:FilterProps){
     const{manufacturer,year,model,limit,fuel}=filters
     const headers = {
-        'X-RapidAPI-Key': '8268286977msh61042d62a980f21p106a9ejsn78a2f459c003',
+        'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
     const response = await  fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&&year=${year}&&model=${model}&&limit=${limit}&fuel_type=${fuel}`,{headers:headers})
